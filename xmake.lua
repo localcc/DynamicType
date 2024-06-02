4530a1e2a44299c87bb3f6fbb820634b1513fe21
localcc <work@localcc.cc>
2024-06-02 21:06:38 +0200

    chore: make lib headeronly

--- xmake.lua
+++ xmake.lua
@@ -5,14 +5,12 @@ includes("deps")
 add_requires("reflect")
 
 target("DynamicType")
-    set_kind("binary")
+    set_kind("headeronly")
     
     set_languages("cxx23")
 
     add_headerfiles("include/**.hpp", { public = true })
     add_includedirs("include", { public = true })
-
-    add_files("src/*.cpp")
 
     add_extrafiles(".clang-format")
 
